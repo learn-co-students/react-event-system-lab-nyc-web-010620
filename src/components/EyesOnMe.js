a829b67bae6053db6fd3e05c502e278d94c65c32
alex.szeto <alex.szeto.a@gmail.com>
2020-03-10 08:40:49 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+class EyesOnMe extends React.Component{
+    constructor(){
+        super()
+    }
+
+    whenFocused = () => console.log("Good!")
+    whenBlur = () => console.log("Hey! Eyes on me!")
+
+    render(){
+        return(
+            <button onFocus={this.whenFocused} onBlur={this.whenBlur}>Hi</button>
+        )
+    }
+
+
+}
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from "react"
+
+class Keypad extends React.Component{
+    constructor(){
+        super()
+    }
+
+    abstracting = () => console.log("Entering password...")
+
+    render(){
+        return(
+            <input type="password" onKeyUp={this.abstracting}/>
+        )
+    }
+
+
+}
+
+
+export default Keypad
